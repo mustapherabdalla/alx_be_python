db874b29cdfb4167d83dacbe7639a34a287f9a00
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-07-16 11:34:35 +0300

    Added daily_reminder.py file for reminding someone on their daily tasks

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -10,23 +10,18 @@ while True:
                 if time_bound == "yes":
                     print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
                 else:
-                    print(
-                        f"Reminder: '{task}' is a high-priority task that requires your attention soon. While it doesn’t have a strict deadline, "
+                    print(f"Reminder: '{task}' is a high-priority task that requires your attention soon. While it doesn’t have a strict deadline, "
                         "completing it promptly will help keep your workload manageable.")
             case "medium":
                 if time_bound == "yes":
-                    print(
-                        f"Reminder: '{task}' is a medium-priority task with a deadline. Please ensure you complete it on time to stay on track!")
+                    print(f"Reminder: '{task}' is a medium-priority task with a deadline. Please ensure you complete it on time to stay on track!")
                 else:
-                    print(
-                        f"Reminder: '{task}' is a medium-priority task. While there’s no urgent deadline, Feel free to tackle it whenever it’s convenient for you.")
+                    print(f"Reminder: '{task}' is a medium-priority task. While there’s no urgent deadline, Feel free to tackle it whenever it’s convenient for you.")
             case "low":
                 if time_bound == "yes":
-                    print(
-                        f"Reminder: '{task}' is a low-priority task, but it does have a deadline. Work on it when you can to avoid last-minute delays.")
+                    print(f"Reminder: '{task}' is a low-priority task, but it does have a deadline. Work on it when you can to avoid last-minute delays.")
                 else:
-                    print(
-                        f"Reminder: '{task}' is a low-priority task with no strict deadline. Consider completing it when you have free time.")
+                    print(f"Reminder: '{task}' is a low-priority task with no strict deadline. Consider completing it when you have free time.")
             case _:
                 print("Invalid choice! Please select either high, medium or low.")
     else:
