a7d767ad2e19de7f45d0b4f197b14440e60a29bf
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-07-29 12:36:14 +0300

    Added bank_account.py file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,15 @@
 class BankAccount:
-    def __init__(self, account_balance, initial_balance = 0):
+    def __init__(self, account_balance = 0):
         self.account_balance = account_balance
-        self.initial_balance = initial_balance
 
     def deposit(self, amount):
-        self.initial_balance = self.account_balance + amount
+        self.account_balance = self.account_balance + amount
 
     def withdraw(self, amount):
         if self.account_balance > amount:
-            self.initial_balance = self.account_balance - amount
+            self.account_balance = self.account_balance - amount
             return True
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.initial_balance}")
+        print(f"Current Balance: ${self.account_balance}")
