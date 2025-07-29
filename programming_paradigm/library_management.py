92c5e7ffae34901d4f4c5d3a1400383412d047a3
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-07-29 19:11:34 +0300

    Added library_management.py file for handling basic library operations

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,32 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+        self.book = None
+
+    def add_book(self, book: Book):
+        if book is not None:
+            self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                self.book = book
+                self._books.remove(book)
+                book._is_checked_out = True
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title != title:
+                self._books.append(self.book)
+                self.book._is_checked_out = False
+
+    def list_available_books(self):
+        for book in self._books:
+            print(f"{book.title} by {book.author}")
